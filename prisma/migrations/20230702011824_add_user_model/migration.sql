850344a6d1381cef0d8fee5563c8dbfb4ac9e376
sc30gsw <haidoushi875@gmail.com>
2023-07-02 10:20:56 +0900

    Userモデルの追加

---
+++ prisma/migrations/20230702011824_add_user_model/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE `Link` ADD COLUMN `userId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Link` ADD CONSTRAINT `Link_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
