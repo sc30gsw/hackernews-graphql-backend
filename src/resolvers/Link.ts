01110d2e060217655beabddef895a8f75e249572
sc30gsw <haidoushi875@gmail.com>
2023-07-04 15:23:54 +0900

    型・フォーマット

--- src/resolvers/Link.ts
+++ src/resolvers/Link.ts
@@ -1,6 +1,6 @@
 import type { Context } from '@/types/Context'
 
-export const user = (parent: { id: any }, __: unknown, context: Context) => {
+export const user = (parent: { id: number }, __: unknown, context: Context) => {
   return context.prisma.link
     .findUnique({
       where: { id: parent.id },
--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,6 +1,10 @@
 import type { Context } from '@/types/Context'
 
-export const links = (parent: { id: any }, __: unknown, context: Context) => {
+export const links = (
+  parent: { id: number },
+  __: unknown,
+  context: Context
+) => {
   return context.prisma.user
     .findUnique({
       where: { id: parent.id },
