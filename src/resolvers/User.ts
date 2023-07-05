eb8264579cdb8c7fc2fc5ce7c9f2446ff2f7a5ef
sc30gsw <haidoushi875@gmail.com>
2023-07-06 07:34:43 +0900

    return文の省略

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,13 +1,8 @@
 import type { Context } from '@/types/Context'
 
-export const links = (
-  parent: { id: number },
-  __: unknown,
-  context: Context
-) => {
-  return context.prisma.user
+export const links = (parent: { id: number }, __: unknown, context: Context) =>
+  context.prisma.user
     .findUnique({
       where: { id: parent.id },
     })
     .links()
-}
